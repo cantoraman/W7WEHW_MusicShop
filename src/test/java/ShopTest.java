9f72930013608eba035dc1bdbec5732dfa155c76
Can Toraman <cantoraman@gmail.com>
2018-07-06 16:23:01 +0100

    adds many to shop and removes from it

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,4 +1,5 @@
 import Instruments.DrumSet;
+import Instruments.Guitar;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,12 +9,17 @@ public class ShopTest {
 
     Shop shop;
     DrumSet drumSet;
+    DrumStick drumStick;
+    Guitar guitar;
 
     @Before
     public void setup(){
         shop = new Shop("Ray's");
         drumSet = new DrumSet("Yamaha", "Loud", "Black", "DrumSound" ,500, 1000, 5);
+        drumStick = new DrumStick("Yamaha", 1, 2);
+        guitar = new Guitar("Fender", "Electro", "Red", "GuitarSound", 1000, 2000, 6);
     }
+
 
     @Test
     public void canAddItems(){
@@ -27,6 +33,15 @@ public class ShopTest {
         assertEquals(0, shop.getStockSize());
     }
 
+    @Test
+    public void canAddRemoveMultipleItems(){
+        shop.addItem(drumSet);
+        shop.addItem(drumStick);
+        shop.addItem(guitar);
+        shop.removeItem(drumStick);
+        assertEquals(2, shop.getStockSize());
+
+    }
 
 
 
